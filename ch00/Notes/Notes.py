d3410968618fba51701b1eb81dcf7da764861c41
Kenny Romano <kromano1@binghamton.edu>
2023-02-20 20:25:46 -0500

    Lab 2

--- ch00/Notes/Notes.py
+++ ch00/Notes/Notes.py
@@ -151,4 +151,58 @@ pygame.draw.rect(screen, "pink", dimensions)
 #Exercise: drawing with pygame
 screen_size = screen.get_size()
 dimensions = [screen_size[0] / 2, screen_size[1] / 2, 250, 250]
+random.randrange(1, 10)
+#Generates Random Number from 1-9 (does not include the upper-limit value)
 
+## Events
+## Operating system handles events
+## Your Program -> OS: Anything happens
+
+##OS => event
+##type of event == operation 
+# - click 
+# - key_presses
+
+# connect actions to algorithms 
+#Simon Says
+
+import pygame
+import random
+pygame.init()
+screen = pygame.display.set_mode()
+colors = ["red", "green", "blue", "yellow"]
+random.shuffle(colors)
+
+for color in colors:
+    screen.fill(color)
+    pygame.display.flip()
+    pygame.time.wait(1000)
+
+    screen.fill("black")
+    pygame.display.flip()
+    pygame.time.wait(2000)
+
+    message = """
+       Simon says:
+       UP: RED
+       DOWN: BLUE
+       LEFT: GREEN
+       RIGHTL YELLOW
+
+       click on the window, enter sequence, m then press enter
+"""
+response = input(message)
+
+#pygame.EVENT_OBJECT
+
+for event in pygame.event.get():
+    print(event)
+
+for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+         if event.key == pygame.K_UP:
+             screen.fill("red")
+             user_sequence.append("red")
+        elif event.key == pygame.K_DOWN:
+             screen.fill("blue")
+             user_sequence.append("blue")
